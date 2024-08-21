f8f7d69d09feb8e0bcc1acc7b0abef3c89e8090c
shahinjaffer07 <shahinjaffer07@gmail.com>
2024-08-21 16:17:06 +0530

    Success

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+from datetime import datetime
+s=socket.socket()
+s.bind(('localhost',8000))
+s.listen(5)
+c,addr=s.accept()
+print("Client Address : ",addr)
+now = datetime.now()
+c.send(now.strftime("%d/%m/%Y %H:%M:%S").encode())
+ack=c.recv(1024).decode()
+if ack:
+ print(ack)
+c.close()
---
+++ server.py
@@ -0,0 +1,6 @@
+import socket
+s=socket.socket()
+s.connect(('localhost',8000))
+print(s.getsockname())
+print(s.recv(1024).decode())
+s.send("acknowledgement recived from the server".encode())
